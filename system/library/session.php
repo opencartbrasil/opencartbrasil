731c030450bb9a81de129006194f244475ac13d6
v5manoel <manoel.vidal@opencartbrasil.com.br>
2018-09-17 00:05:22 -0300

    Correção na biblioteca de sessão

--- system/library/session.php
+++ system/library/session.php
@@ -1,90 +1,81 @@
 <?php
-/**
- * @package		OpenCart
- * @author		Daniel Kerr
- * @copyright	Copyright (c) 2005 - 2017, OpenCart, Ltd. (https://www.opencart.com/)
- * @license		https://opensource.org/licenses/GPL-3.0
- * @link		https://www.opencart.com
-*/
+namespace Session;
+class File {
+	private $directory;
 
-/**
-* Session class
-*/
-class Session {
-	protected $adaptor;
-	protected $session_id;
-	public $data = array();
+	public function read($session_id) {
+		$file = DIR_SESSION . 'sess_' . basename($session_id);
 
-	/**
-	 * Constructor
-	 *
-	 * @param	string	$adaptor
-	 * @param	object	$registry
- 	*/
-	public function __construct($adaptor, $registry = '') {
-		$class = 'Session\\' . $adaptor;
-		
-		if (class_exists($class)) {
-			if ($registry) {
-				$this->adaptor = new $class($registry);
-			} else {
-				$this->adaptor = new $class();
-			}	
-			
-			register_shutdown_function(array($this, 'close'));
-		} else {
-			trigger_error('Error: Could not load cache adaptor ' . $adaptor . ' session!');
-			exit();
-		}	
-	}
-	
-	/**
-	 * 
-	 *
-	 * @return	string
- 	*/	
-	public function getId() {
-		return $this->session_id;
-	}
+		if (is_file($file)) {
+			$size = filesize($file);
 
-	/**
-	 *
-	 *
-	 * @param	string	$session_id
-	 *
-	 * @return	string
- 	*/	
-	public function start($session_id = '') {
-		if (!$session_id) {
-			if (function_exists('random_bytes')) {
-				$session_id = substr(bin2hex(random_bytes(26)), 0, 26);
-			} else {
-				$session_id = substr(bin2hex(openssl_random_pseudo_bytes(26)), 0, 26);
+			if ($size) {
+				$handle = fopen($file, 'r');
+
+				flock($handle, LOCK_SH);
+
+				$data = fread($handle, $size);
+
+				flock($handle, LOCK_UN);
+
+				fclose($handle);
+
+				return unserialize($data);
 			}
 		}
 
-		if (preg_match('/^[a-zA-Z0-9,\-]{22,52}$/', $session_id)) {
-			$this->session_id = $session_id;
-		} else {
-			exit('Error: Invalid session ID!');
+		return array();
+	}
+
+	public function write($session_id, $data) {
+		$file = DIR_SESSION . 'sess_' . basename($session_id);
+
+		$handle = fopen($file, 'w');
+
+		flock($handle, LOCK_EX);
+
+		fwrite($handle, serialize($data));
+
+		fflush($handle);
+
+		flock($handle, LOCK_UN);
+
+		fclose($handle);
+
+		return true;
+	}
+
+	public function destroy($session_id) {
+		$file = DIR_SESSION . 'sess_' . basename($session_id);
+
+		if (is_file($file)) {
+			unlink($file);
 		}
-		
-		$this->data = $this->adaptor->read($session_id);
-		
-		return $session_id;
 	}
-	
-	/**
-	 * 
- 	*/
-	public function close() {
-		$this->adaptor->write($this->session_id, $this->data);
-	}
-	
-	/**
-	 * 
- 	*/	
-	public function __destroy() {
-		$this->adaptor->destroy($this->session_id);
+
+	public function __destruct() {
+		if (ini_get('session.gc_divisor')) {
+			$gc_divisor = ini_get('session.gc_divisor');
+		} else {
+			$gc_divisor = 1;
+		}
+
+		if (ini_get('session.gc_probability')) {
+			$gc_probability = ini_get('session.gc_probability');
+		} else {
+			$gc_probability = 1;
+		}
+
+		if ((rand() % $gc_divisor) < $gc_probability) {
+			$expire = time() - ini_get('session.gc_maxlifetime');
+
+			$files = glob(DIR_SESSION . 'sess_*');
+
+			foreach ($files as $file) {
+				if (filemtime($file) < $expire) {
+					unlink($file);
+				}
+			}
+		}
 	}
 }
