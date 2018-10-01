0b49afc1c7f8113fe9fb6a95fe3537ae479f94c3
Rafael Mancini Silva <rafael.mancini@amfweb.com.br>
2018-10-01 17:35:39 -0300

    Módulo Frenet para cálculo de fretes

---
+++ catalog/language/pt-br/extension/shipping/frenet.php
@@ -0,0 +1,7 @@
+<?php
+$_['text_title']  = 'Frenet';
+$_['text_sucesso']= 'Reconexão realizada com sucesso!';
+$_['text_free']   = '<span style="color:green;font-weight:bold">Grátis</span>';
+
+$_['error_conexao']  	= 'Não foi possível estabelecer conexão com os Frenet. Tentando reconectar...';
+$_['error_reconexao']  	= 'Falha na tentativa de reconectar com os Frenet. O WebService dos Frenet apresenta instabilidade ou está fora do ar.';
