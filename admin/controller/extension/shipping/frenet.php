8e21fa1d9708ef3a83bb1a0ba83289c00c3684f0
v5manoel <manoel.vidal@opencartbrasil.com.br>
2018-10-01 23:58:26 -0300

    Melhorias na extensão Frenet

--- admin/controller/extension/shipping/frenet.php
+++ admin/controller/extension/shipping/frenet.php
@@ -1,59 +1,29 @@
 <?php
 class ControllerExtensionShippingFrenet extends Controller {
-	private $error = array(); 
-	
-	public function index() {   
-		$this->load->language('extension/shipping/frenet');
-		
+	private $error = array();
+
+	public function index() {
+		$data = $this->load->language('extension/shipping/frenet');
+
 		$this->document->setTitle($this->language->get('heading_title'));
-		
+
 		$this->load->model('setting/setting');
-		
+
 		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
-			$this->model_setting_setting->editSetting('shipping_frenet', $this->request->post);		
-			
+			$this->model_setting_setting->editSetting('shipping_frenet', $this->request->post);
+
 			$this->session->data['success'] = $this->language->get('text_success');
 
-            $this->response->redirect($this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=shipping', true));
+			$this->response->redirect($this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=shipping', true));
 
 		}
-		
-		$data['heading_title'] = $this->language->get('heading_title');
-		
-		$data['text_edit'] = $this->language->get('text_edit');
-		$data['text_enabled'] = $this->language->get('text_enabled');
-		$data['text_disabled'] = $this->language->get('text_disabled');
-		$data['text_none'] = $this->language->get('text_none');
-		$data['text_yes'] = $this->language->get('text_yes');
-		$data['text_no'] = $this->language->get('text_no');
-		$data['text_select_all'] = $this->language->get('text_select_all');
-		$data['text_unselect_all'] = $this->language->get('text_unselect_all');
-
-        $data['entry_msg_prazo'] = $this->language->get('entry_msg_prazo');
-        $data['entry_frenet_key'] = $this->language->get('entry_frenet_key');
-        $data['entry_frenet_key_codigo'] = $this->language->get('entry_frenet_key_codigo');
-        $data['entry_frenet_key_senha'] = $this->language->get('entry_frenet_key_senha');
-
-		$data['entry_cost'] = $this->language->get('entry_cost');
-		$data['entry_status'] = $this->language->get('entry_status');
-		$data['entry_sort_order'] = $this->language->get('entry_sort_order');
-
-
-		$data['help_frenet_key'] = $this->language->get('help_frenet_key');
-        $data['help_msg_prazo'] = $this->language->get('help_msg_prazo');
-		
-		$data['button_save'] = $this->language->get('button_save');
-		$data['button_cancel'] = $this->language->get('button_cancel');
-		
-		$data['tab_general'] = $this->language->get('tab_general');
-		
-		$data['entry_postcode']= $this->language->get('entry_postcode');
 
 		if (isset($this->error['warning'])) {
 			$data['error_warning'] = $this->error['warning'];
 		} else {
 			$data['error_warning'] = '';
 		}
+
 		if (isset($this->error['postcode'])) {
 			$data['error_postcode'] = $this->error['postcode'];
 		} else {
@@ -62,22 +32,22 @@ class ControllerExtensionShippingFrenet extends Controller {
 
 		$data['breadcrumbs'] = array();
 
-        $data['breadcrumbs'][] = array(
-            'text' => $this->language->get('text_home'),
-            'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], true)
-        );
-        $data['breadcrumbs'][] = array(
-            'text' => $this->language->get('text_shipping'),
-            'href' => $this->url->link('extension/extension', 'user_token=' . $this->session->data['user_token'], true)
-        );
-        $data['breadcrumbs'][] = array(
-            'text' => $this->language->get('heading_title'),
-            'href' => $this->url->link('extension/shipping/frenet', 'user_token=' . $this->session->data['user_token'], true)
-        );
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_home'),
+			'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], true)
+		);
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_shipping'),
+			'href' => $this->url->link('extension/extension', 'user_token=' . $this->session->data['user_token'], true)
+		);
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('heading_title'),
+			'href' => $this->url->link('extension/shipping/frenet', 'user_token=' . $this->session->data['user_token'], true)
+		);
 
-        $data['action'] = $this->url->link('extension/shipping/frenet', 'user_token=' . $this->session->data['user_token'], true);
+		$data['action'] = $this->url->link('extension/shipping/frenet', 'user_token=' . $this->session->data['user_token'], true);
 
-        $data['cancel'] = $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=shipping', true);
+		$data['cancel'] = $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=shipping', true);
 
 		if (isset($this->request->post['shipping_frenet_status'])) {
 			$data['shipping_frenet_status'] = $this->request->post['shipping_frenet_status'];
@@ -90,22 +60,24 @@ class ControllerExtensionShippingFrenet extends Controller {
 		} else {
 			$data['shipping_frenet_postcode'] = $this->config->get('shipping_frenet_postcode');
 		}
-        if (isset($this->request->post['shipping_frenet_msg_prazo'])) {
-            $data['shipping_frenet_msg_prazo'] = $this->request->post['shipping_frenet_msg_prazo'];
-        } else {
-            $data['shipping_frenet_msg_prazo'] = $this->config->get('shipping_frenet_msg_prazo');
-        }
+
+		if (isset($this->request->post['shipping_frenet_msg_prazo'])) {
+			$data['shipping_frenet_msg_prazo'] = $this->request->post['shipping_frenet_msg_prazo'];
+		} else {
+			$data['shipping_frenet_msg_prazo'] = $this->config->get('shipping_frenet_msg_prazo');
+		}
 
 		if (isset($this->request->post['shipping_frenet_contrato_codigo'])) {
 			$data['shipping_frenet_contrato_codigo'] = $this->request->post['shipping_frenet_contrato_codigo'];
 		} else {
 			$data['shipping_frenet_contrato_codigo'] = $this->config->get('shipping_frenet_contrato_codigo');
 		}
+
 		if (isset($this->request->post['shipping_frenet_contrato_senha'])) {
 			$data['shipping_frenet_contrato_senha'] = $this->request->post['shipping_frenet_contrato_senha'];
 		} else {
 			$data['shipping_frenet_contrato_senha'] = $this->config->get('shipping_frenet_contrato_senha');
-		}						
+		}
 
 		if (isset($this->request->post['shipping_frenet_sort_order'])) {
 			$data['shipping_frenet_sort_order'] = $this->request->post['shipping_frenet_sort_order'];
@@ -117,16 +89,15 @@ class ControllerExtensionShippingFrenet extends Controller {
 		$data['column_left'] = $this->load->controller('common/column_left');
 		$data['footer'] = $this->load->controller('common/footer');
 
-        $this->response->setOutput($this->load->view('extension/shipping/frenet', $data));
+		$this->response->setOutput($this->load->view('extension/shipping/frenet', $data));
 
 	}
-	
+
 	protected function validate() {
-        if (!$this->user->hasPermission('modify', 'extension/shipping/frenet')) {
-            $this->error['warning'] = $this->language->get('error_permission');
-        }
+		if (!$this->user->hasPermission('modify', 'extension/shipping/frenet')) {
+			$this->error['warning'] = $this->language->get('error_permission');
+		}
 
 		return !$this->error;
 	}
 }
-?>
--- admin/language/en-gb/extension/shipping/frenet.php
+++ admin/language/en-gb/extension/shipping/frenet.php
@@ -1,30 +1,27 @@
 <?php
 // Heading
-$_['heading_title']       = 'Frenet';
+$_['heading_title']           = 'Frenet';
 
 // Text
-$_['text_enabled']        = 'Habilitado';
-$_['text_disabled']       = 'Desabilitado';
-$_['text_edit']           = 'Editar o módulo Frenet';
-$_['text_shipping']       = 'Formas de Envio';
-$_['text_success']        = 'Módulo Frenet atualizado com sucesso!';
+$_['text_extension']          = 'Extensões';
+$_['text_success']            = 'Frete Frenet modificado com sucesso!';
+$_['text_edit']               = 'Editando Frenet';
 
 // Entry
-$_['entry_status']        = 'Situação';
-$_['entry_tax_class']  	  = 'Grupo de Impostos';
-$_['entry_geo_zone']      = 'Região Geográfica';
-$_['entry_postcode']      = 'CEP de origem';
-$_['entry_sort_order']    = 'Ordem';
-$_['entry_frenet_key']    		= 'Chave de acesso';
-$_['entry_msg_prazo']    		= 'Mensagem que exibe prazo de entrega';
-$_['entry_frenet_key_codigo']   	= 'Chave';
-$_['entry_frenet_key_senha']    	= 'Senha';
+$_['entry_tax_class']         = 'Grupo de Impostos';
+$_['entry_geo_zone']          = 'Região Geográfica';
+$_['entry_status']            = 'Situação';
+$_['entry_postcode']          = 'CEP de origem';
+$_['entry_sort_order']        = 'Posição';
+$_['entry_frenet_key']        = 'Chave de acesso';
+$_['entry_msg_prazo']         = 'Mensagem que exibe prazo de entrega';
+$_['entry_frenet_key_codigo'] = 'Chave';
+$_['entry_frenet_key_senha']  = 'Senha';
 
 // Help
-$_['help_frenet_key'] 			= 'Chave e senha de acesso ao webservice do Frenet.';
-$_['help_msg_prazo']    		= '%s - Em média %d dia(s)';
+$_['help_frenet_key']         = 'Chave e senha de acesso ao webservice do Frenet.';
+$_['help_msg_prazo']          = '%s - Em média %d dia(s)';
 
 // Error
-$_['error_permission']    = 'Atenção: Você não possui permissão para modificar o módulo Frenet!';
-$_['error_postcode']      = 'Formato inválido de CEP!';
-?>
+$_['error_permission']        = 'Atenção: Você não tem permissão para modificar a extensão Frenet!';
+$_['error_postcode']          = 'Formato inválido de CEP!';
