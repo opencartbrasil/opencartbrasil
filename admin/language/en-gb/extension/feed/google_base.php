9dc23b3e9d0bf7ca3c83935390e289dbb8b7857c
v5manoel <manoel.vidal@opencartbrasil.com.br>
2018-11-14 21:15:18 -0300

    Correções na tradução da extensão Google Merchant Center

--- admin/language/en-gb/extension/feed/google_base.php
+++ admin/language/en-gb/extension/feed/google_base.php
@@ -1,11 +1,11 @@
 <?php
 // Heading
-$_['heading_title']          = 'Google Base';
+$_['heading_title']          = 'Google Merchant Center';
 
 // Text
 $_['text_extension']         = 'Extensions';
-$_['text_success']           = 'Success: You have modified Google Base feed!';
-$_['text_edit']              = 'Edit Google Base';
+$_['text_success']           = 'Success: You have modified Google Merchant Center feed!';
+$_['text_edit']              = 'Edit Google Merchant Center';
 $_['text_import']            = 'To download the latest Google category list by <a href="https://support.google.com/merchants/answer/160081?hl=en" target="_blank" class="alert-link">clicking here</a> and choose taxonomy with numeric IDs in Plain Text (.txt) file. Upload via the green import button.';
 
 // Column
@@ -20,6 +20,6 @@ $_['entry_data_feed']        = 'Data Feed Url';
 $_['entry_status']           = 'Status';
 
 // Error
-$_['error_permission']       = 'Warning: You do not have permission to modify Google Base feed!';
+$_['error_permission']       = 'Warning: You do not have permission to modify Google Merchant Center feed!';
 $_['error_upload']           = 'File could not be uploaded!';
 $_['error_filetype']         = 'Invalid file type!';
--- admin/language/pt-br/extension/feed/google_base.php
+++ admin/language/pt-br/extension/feed/google_base.php
@@ -1,25 +1,25 @@
 <?php
 // Heading
-$_['heading_title']          = 'Google Merchante Center';
+$_['heading_title']          = 'Google Merchant Center';
 
 // Text
 $_['text_extension']         = 'Extensões';
-$_['text_success']           = 'Google Merchante Center modificado com sucesso!';
-$_['text_edit']              = 'Editando Google Merchante Center';
-$_['text_import']            = 'Para baixar a última lista de categorias <a href="https://support.google.com/merchants/answer/160081?hl=pt-br" target="_blank" class="alert-link">clique aqui</a> e escolha a taxonomia com IDs numéricos em arquivo de texto simples (.txt). Depois carregue através do botão de importação.';
+$_['text_success']           = 'Google Merchant Center modificado com sucesso!';
+$_['text_edit']              = 'Editando Google Merchant Center';
+$_['text_import']            = 'Antes de habilitar a extensão, baixe a lista de categorias do Google <a href="https://support.google.com/merchants/answer/160081?hl=pt-br" target="_blank" class="alert-link">clicando aqui</a> e escolha a taxonomia com IDs numéricos em arquivo de Texto simples (.txt). Depois carregue o arquivo na loja através do botão de importação, que fica acima na cor verde.';
 
 // Column
-$_['column_google_category'] = 'Categoria Google';
-$_['column_category']        = 'Categoria';
+$_['column_google_category'] = 'Categoria no Google';
+$_['column_category']        = 'Departamento na loja';
 $_['column_action']          = 'Ação';
 
 // Entry
-$_['entry_google_category']  = 'Categoria Google';
-$_['entry_category']         = 'Categoria';
+$_['entry_google_category']  = 'Categoria no Google';
+$_['entry_category']         = 'Departamento na loja';
 $_['entry_data_feed']        = 'URL';
 $_['entry_status']           = 'Situação';
 
 // Error
-$_['error_permission']       = 'Atenção: Você não possui permissão para modificar a extensão Google Merchante Center!';
+$_['error_permission']       = 'Atenção: Você não possui permissão para modificar a extensão Google Merchant Center!';
 $_['error_upload']           = 'O arquivo não pôde ser carregado!';
-$_['error_filetype']         = 'O tipo de arquivo é inválido!';
+$_['error_filetype']         = 'O tipo de arquivo não é válido!';
