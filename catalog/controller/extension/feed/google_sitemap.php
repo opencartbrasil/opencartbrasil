1eb33cfaf1c9b87a8dfc000da2126576a151a3a6
v5manoel <manoel.vidal@opencartbrasil.com.br>
2018-11-18 23:11:09 -0300

    Melhorias na extensão Google Sitemap

--- catalog/controller/extension/feed/google_sitemap.php
+++ catalog/controller/extension/feed/google_sitemap.php
@@ -2,8 +2,8 @@
 class ControllerExtensionFeedGoogleSitemap extends Controller {
 	public function index() {
 		if ($this->config->get('feed_google_sitemap_status')) {
-			$output  = '<?xml version="1.0" encoding="UTF-8"?>';
-			$output .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">';
+			$output  = '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL;
+			$output .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">' . PHP_EOL;
 
 			$this->load->model('catalog/product');
 			$this->load->model('tool/image');
@@ -12,17 +12,17 @@ class ControllerExtensionFeedGoogleSitemap extends Controller {
 
 			foreach ($products as $product) {
 				if ($product['image']) {
-					$output .= '<url>';
-					$output .= '  <loc>' . $this->url->link('product/product', 'product_id=' . $product['product_id']) . '</loc>';
-					$output .= '  <changefreq>weekly</changefreq>';
-					$output .= '  <lastmod>' . date('Y-m-d\TH:i:sP', strtotime($product['date_modified'])) . '</lastmod>';
-					$output .= '  <priority>1.0</priority>';
-					$output .= '  <image:image>';
-					$output .= '  <image:loc>' . $this->model_tool_image->resize($product['image'], $this->config->get('theme_' . $this->config->get('config_theme') . '_image_popup_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_popup_height')) . '</image:loc>';
-					$output .= '  <image:caption>' . $product['name'] . '</image:caption>';
-					$output .= '  <image:title>' . $product['name'] . '</image:title>';
-					$output .= '  </image:image>';
-					$output .= '</url>';
+					$output .= '<url>' . PHP_EOL;
+					$output .= '  <loc>' . $this->url->link('product/product', 'product_id=' . $product['product_id']) . '</loc>' . PHP_EOL;
+					$output .= '  <changefreq>weekly</changefreq>' . PHP_EOL;
+					$output .= '  <lastmod>' . date('Y-m-d\TH:i:sP', strtotime($product['date_modified'])) . '</lastmod>' . PHP_EOL;
+					$output .= '  <priority>1.0</priority>' . PHP_EOL;
+					$output .= '  <image:image>' . PHP_EOL;
+					$output .= '  <image:loc>' . $this->model_tool_image->resize($product['image'], $this->config->get('theme_' . $this->config->get('config_theme') . '_image_popup_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_popup_height')) . '</image:loc>' . PHP_EOL;
+					$output .= '  <image:caption>' . $product['name'] . '</image:caption>' . PHP_EOL;
+					$output .= '  <image:title>' . $product['name'] . '</image:title>' . PHP_EOL;
+					$output .= '  </image:image>' . PHP_EOL;
+					$output .= '</url>' . PHP_EOL;
 				}
 			}
 
@@ -35,20 +35,20 @@ class ControllerExtensionFeedGoogleSitemap extends Controller {
 			$manufacturers = $this->model_catalog_manufacturer->getManufacturers();
 
 			foreach ($manufacturers as $manufacturer) {
-				$output .= '<url>';
-				$output .= '  <loc>' . $this->url->link('product/manufacturer/info', 'manufacturer_id=' . $manufacturer['manufacturer_id']) . '</loc>';
-				$output .= '  <changefreq>weekly</changefreq>';
-				$output .= '  <priority>0.7</priority>';
-				$output .= '</url>';
+				$output .= '<url>' . PHP_EOL;
+				$output .= '  <loc>' . $this->url->link('product/manufacturer/info', 'manufacturer_id=' . $manufacturer['manufacturer_id']) . '</loc>' . PHP_EOL;
+				$output .= '  <changefreq>weekly</changefreq>' . PHP_EOL;
+				$output .= '  <priority>0.7</priority>' . PHP_EOL;
+				$output .= '</url>' . PHP_EOL;
 
 				$products = $this->model_catalog_product->getProducts(array('filter_manufacturer_id' => $manufacturer['manufacturer_id']));
 
 				foreach ($products as $product) {
-					$output .= '<url>';
-					$output .= '  <loc>' . $this->url->link('product/product', 'manufacturer_id=' . $manufacturer['manufacturer_id'] . '&product_id=' . $product['product_id']) . '</loc>';
-					$output .= '  <changefreq>weekly</changefreq>';
-					$output .= '  <priority>1.0</priority>';
-					$output .= '</url>';
+					$output .= '<url>' . PHP_EOL;
+					$output .= '  <loc>' . $this->url->link('product/product', 'manufacturer_id=' . $manufacturer['manufacturer_id'] . '&product_id=' . $product['product_id']) . '</loc>' . PHP_EOL;
+					$output .= '  <changefreq>weekly</changefreq>' . PHP_EOL;
+					$output .= '  <priority>1.0</priority>' . PHP_EOL;
+					$output .= '</url>' . PHP_EOL;
 				}
 			}
 
@@ -57,14 +57,14 @@ class ControllerExtensionFeedGoogleSitemap extends Controller {
 			$informations = $this->model_catalog_information->getInformations();
 
 			foreach ($informations as $information) {
-				$output .= '<url>';
-				$output .= '  <loc>' . $this->url->link('information/information', 'information_id=' . $information['information_id']) . '</loc>';
-				$output .= '  <changefreq>weekly</changefreq>';
-				$output .= '  <priority>0.5</priority>';
-				$output .= '</url>';
+				$output .= '<url>' . PHP_EOL;
+				$output .= '  <loc>' . $this->url->link('information/information', 'information_id=' . $information['information_id']) . '</loc>' . PHP_EOL;
+				$output .= '  <changefreq>weekly</changefreq>' . PHP_EOL;
+				$output .= '  <priority>0.5</priority>' . PHP_EOL;
+				$output .= '</url>' . PHP_EOL;
 			}
 
-			$output .= '</urlset>';
+			$output .= '</urlset>' . PHP_EOL;
 
 			$this->response->addHeader('Content-Type: application/xml');
 			$this->response->setOutput($output);
@@ -83,20 +83,20 @@ class ControllerExtensionFeedGoogleSitemap extends Controller {
 				$new_path = $current_path . '_' . $result['category_id'];
 			}
 
-			$output .= '<url>';
-			$output .= '  <loc>' . $this->url->link('product/category', 'path=' . $new_path) . '</loc>';
-			$output .= '  <changefreq>weekly</changefreq>';
-			$output .= '  <priority>0.7</priority>';
-			$output .= '</url>';
+			$output .= '<url>' . PHP_EOL;
+			$output .= '  <loc>' . $this->url->link('product/category', 'path=' . $new_path) . '</loc>' . PHP_EOL;
+			$output .= '  <changefreq>weekly</changefreq>' . PHP_EOL;
+			$output .= '  <priority>0.7</priority>' . PHP_EOL;
+			$output .= '</url>' . PHP_EOL;
 
 			$products = $this->model_catalog_product->getProducts(array('filter_category_id' => $result['category_id']));
 
 			foreach ($products as $product) {
-				$output .= '<url>';
-				$output .= '  <loc>' . $this->url->link('product/product', 'path=' . $new_path . '&product_id=' . $product['product_id']) . '</loc>';
-				$output .= '  <changefreq>weekly</changefreq>';
-				$output .= '  <priority>1.0</priority>';
-				$output .= '</url>';
+				$output .= '<url>' . PHP_EOL;
+				$output .= '  <loc>' . $this->url->link('product/product', 'path=' . $new_path . '&product_id=' . $product['product_id']) . '</loc>' . PHP_EOL;
+				$output .= '  <changefreq>weekly</changefreq>' . PHP_EOL;
+				$output .= '  <priority>1.0</priority>' . PHP_EOL;
+				$output .= '</url>' . PHP_EOL;
 			}
 
 			$output .= $this->getCategories($result['category_id'], $new_path);
