4af9d4f410f0f5f7c79a1b9414f5bb039ace6847
v5manoel <manoel.vidal@opencartbrasil.com.br>
2018-12-02 05:06:47 -0300

    Melhorias na captura de IP pela API

--- catalog/controller/api/login.php
+++ catalog/controller/api/login.php
@@ -20,8 +20,34 @@ class ControllerApiLogin extends Controller {
 				$ip_data[] = trim($result['ip']);
 			}
 
-			if (!in_array($this->request->server['REMOTE_ADDR'], $ip_data)) {
-				$json['error']['ip'] = sprintf($this->language->get('error_ip'), $this->request->server['REMOTE_ADDR']);
+			$ip = '';
+
+			if (isset($_SERVER['REMOTE_ADDR']) && filter_var($_SERVER['REMOTE_ADDR'], FILTER_VALIDATE_IP)) {
+				$ip = $_SERVER['REMOTE_ADDR'];
+			}
+
+			if(isset($_SERVER['HTTP_CF_CONNECTING_IP']) && filter_var($_SERVER['HTTP_CF_CONNECTING_IP'], FILTER_VALIDATE_IP)){
+				$ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
+			}
+
+			if(isset($_SERVER['HTTP_INCAP_CLIENT_IP']) && filter_var($_SERVER['HTTP_INCAP_CLIENT_IP'], FILTER_VALIDATE_IP)){
+				$ip = $_SERVER['HTTP_INCAP_CLIENT_IP'];
+			}
+
+			if(isset($_SERVER['HTTP_X_SUCURI_CLIENTIP']) && filter_var($_SERVER['HTTP_X_SUCURI_CLIENTIP'], FILTER_VALIDATE_IP)){
+				$ip = $_SERVER['HTTP_X_SUCURI_CLIENTIP'];
+			}
+
+			if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+				$xip = trim(current(explode(',', $_SERVER['HTTP_X_FORWARDED_FOR'])));
+
+				if (filter_var($xip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE)) {
+					$ip = $xip;
+				}
+			}
+
+			if (!in_array($ip, $ip_data)) {
+				$json['error']['ip'] = sprintf($this->language->get('error_ip'), $$ip);
 			}
 
 			if (!$json) {
@@ -30,7 +56,7 @@ class ControllerApiLogin extends Controller {
 				$session = new Session($this->config->get('session_engine'), $this->registry);
 				$session->start();
 
-				$this->model_account_api->addApiSession($api_info['api_id'], $session->getId(), $this->request->server['REMOTE_ADDR']);
+				$this->model_account_api->addApiSession($api_info['api_id'], $session->getId(), $$ip);
 
 				$session->data['api_id'] = $api_info['api_id'];
 
