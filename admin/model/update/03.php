b4f45aa979828ba88f5022041c01b5c31f2324fd
v5manoel <manoel.vidal@opencartbrasil.com.br>
2018-12-02 03:46:01 -0300

    Inclusão da coluna SKU nas opções do produto

--- admin/model/update/03.php
+++ admin/model/update/03.php
@@ -1,6 +1,11 @@
 <?php
 class ModelUpdate03 extends Model {
 	public function update() {
+		$table_query = $this->db->query("SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '" . DB_DATABASE . "' AND TABLE_NAME = '" . DB_PREFIX . "product_option_value' AND COLUMN_NAME = 'sku'");
+		if (!$table_query->num_rows) {
+			$this->db->query("ALTER TABLE `" . DB_PREFIX . "product_option_value` ADD `sku` VARCHAR(64) NOT NULL AFTER `option_value_id`;");
+		}
+
 		$table_query = $this->db->query("SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '" . DB_DATABASE . "' AND TABLE_NAME = '" . DB_PREFIX . "customer_ip' AND COLUMN_NAME = 'store_id'");
 		if (!$table_query->num_rows) {
 			$this->db->query("ALTER TABLE `" . DB_PREFIX . "customer_ip` ADD `store_id` INT(11) NOT NULL AFTER `customer_id`;");
