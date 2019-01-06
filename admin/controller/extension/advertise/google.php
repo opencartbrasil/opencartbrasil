6ac6d826579f308b7bc18c7fa17ded623cddc96c
v5manoel <manoel.vidal@opencartbrasil.com.br>
2019-01-06 06:18:34 -0300

    Correção na extensão Google Shopping v105

--- admin/controller/extension/advertise/google.php
+++ admin/controller/extension/advertise/google.php
@@ -1659,7 +1659,7 @@ class ControllerExtensionAdvertiseGoogle extends Controller {
         $this->load->model('extension/advertise/google');
         $this->load->model('catalog/product');
 
-        foreach ($this->model_catalog_product->getProductStores($output) as $store_id)
+        foreach ($this->model_catalog_product->getProductStores($output) as $store_id) {
             $this->model_extension_advertise_google->insertNewProducts(array($output), $store_id);
         }
     }
