d6793531272872b907f174c1462af7057a683987
v5manoel <manoel.vidal@opencartbrasil.com.br>
2019-01-06 06:16:33 -0300

    Inclusão da extensão Google Shopping v105

--- admin/language/en-gb/extension/extension/advertise.php
+++ admin/language/en-gb/extension/extension/advertise.php
@@ -1,16 +1,17 @@
 <?php
-// Heading
-$_['heading_title']    = 'Advertising';
 
-// Text
-$_['text_success']     = 'Success: You have modified advertising!';
-$_['text_list']        = 'Advertising List';
+// Heading
+$_['heading_title'] = "Advertising";
+
 
 // Columns
-$_['column_name']      = 'Advertising Name';
-$_['column_status']    = 'Status';
-$_['column_action']    = 'Action';
+$_['column_name']   = "Advertising Name";
+$_['column_status'] = "Status";
+$_['column_action'] = "Action";
+
+
+// Text
+$_['text_success']   = "<strong>Success:</strong> You have modified advertising!";
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify advertising!';
-$_['error_adblock']    = 'It looks like you are using an ad blocker. In order to use this Advertising section, please disable your ad blocker for your OpenCart admin panel.';
+$_['error_adblock'] = "It looks like you are using an ad blocker. In order to use this Advertising section, please disable your ad blocker for your OpenCart admin panel.";
---
+++ catalog/language/pt-br/extension/advertise/google.php
@@ -0,0 +1,4 @@
+<?php
+// Text
+$_['text_cron_email_message'] = '<p>Este é um relatório automatizado da última tarefa CRON realizada pela sua extensão do Google Shopping.</p><p>%s</p>';
+$_['text_cron_email_subject'] = 'Relatório de tarefa CRON - Google Shopping no OpenCart';
