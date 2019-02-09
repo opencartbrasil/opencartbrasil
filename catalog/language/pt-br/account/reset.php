216e5abf8f55e7e495c9d866d68ae23f6e3b3f64
v5manoel <manoel.vidal@opencartbrasil.com.br>
2019-02-09 01:43:21 -0300

    Melhorias na redefinição da senha do cliente

--- catalog/language/pt-br/account/reset.php
+++ catalog/language/pt-br/account/reset.php
@@ -4,8 +4,8 @@ $_['heading_title']  = 'Redefinir sua senha';
 
 // Text
 $_['text_account']   = 'Minha conta';
-$_['text_password']  = 'Digite a nova senha que você deseja usar.';
-$_['text_success']   = 'Sua senha foi atualizada com sucesso.';
+$_['text_password']  = 'Informe abaixo a nova senha que você deseja utilizar para acessar sua conta em nossa loja.';
+$_['text_success']   = 'Sua senha foi redefinida com sucesso.';
 
 // Entry
 $_['entry_password'] = 'Senha';
@@ -14,4 +14,4 @@ $_['entry_confirm']  = 'Repetir a senha';
 // Error
 $_['error_password'] = 'A senha deve ter entre 4 e 20 caracteres.';
 $_['error_confirm']  = 'A senha repetida está errada.';
-$_['error_code']     = 'O código para redefinir a senha é inválido ou foi já foi utilizado.';
+$_['error_code']     = 'O código para redefinir a senha não é válido ou já foi utilizado.';
