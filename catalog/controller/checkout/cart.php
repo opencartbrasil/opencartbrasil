8588804f4d08577454863778fc5b80cdc087cb50
v5manoel <manoel.vidal@opencartbrasil.com.br>
2019-03-05 15:01:46 -0300

    Remoção de espaço em branco desnecessário

--- catalog/controller/checkout/cart.php
+++ catalog/controller/checkout/cart.php
@@ -438,7 +438,7 @@ class ControllerCheckoutCart extends Controller {
 			$taxes = $this->cart->getTaxes();
 			$total = 0;
 
-			// Because __call can not keep var references so we put them into an array. 			
+			// Because __call can not keep var references so we put them into an array.
 			$total_data = array(
 				'totals' => &$totals,
 				'taxes'  => &$taxes,
