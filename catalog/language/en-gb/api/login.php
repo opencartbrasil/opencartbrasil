b7ca3f2b0db236192ac8ce574e50a92c2ddc0546
v5manoel <manoel.vidal@opencartbrasil.com.br>
2019-03-25 02:18:07 -0300

    Melhorias na API catalog

--- catalog/language/en-gb/api/login.php
+++ catalog/language/en-gb/api/login.php
@@ -1,7 +1,9 @@
 <?php
 // Text
-$_['text_success'] = 'Success: API session successfully started!';
+$_['text_success']  = 'Success: After one hour of inactivity api_token will be deleted.';
 
 // Error
-$_['error_key']    = 'Warning: Incorrect API Key!';
-$_['error_ip']     = 'Warning: Your IP %s is not allowed to access this API!';
+$_['error_method']  = 'Warning: Method not allowed.';
+$_['error_request'] = 'Warning: Missing API Username and API Key.';
+$_['error_key']     = 'Warning: Incorrect API Username and API Key.';
+$_['error_ip']      = 'Warning: Your IP %s is not allowed to access this API.';
--- catalog/language/pt-br/api/login.php
+++ catalog/language/pt-br/api/login.php
@@ -1,7 +1,9 @@
 <?php
 // Text
-$_['text_success'] = 'A sessão da API foi iniciada com sucesso.';
+$_['text_success']  = 'Após uma hora de inatividade o api_token será apagado.';
 
 // Error
-$_['error_key']    = 'Atenção: A chave de acesso da API não é válida.';
-$_['error_ip']     = 'Atenção: O seu IP %s não tem permissão para acessar a API.';
+$_['error_method']  = 'Atenção: O método não permitido.';
+$_['error_request'] = 'Atenção: O usuário e a chave da API não foram informados.';
+$_['error_key']     = 'Atenção: O usuário e a chave da API não são válidos.';
+$_['error_ip']      = 'Atenção: Seu IP %s não tem permissão para acessar a API.';
