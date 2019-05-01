8e7ab42b12ad3bf55340880fa3052063124586a6
v5manoel <manoel.vidal@opencartbrasil.com.br>
2019-05-01 00:24:06 -0300

    Melhorias na estatística de comentários e devoluções

--- admin/controller/event/statistics.php
+++ admin/controller/event/statistics.php
@@ -1,10 +1,24 @@
 <?php
 class ControllerEventStatistics extends Controller {
+	// model/catalog/review/addReview/after
+	public function addReview(&$route, &$args, &$output) {
+		$this->load->model('report/statistics');
+
+		$this->model_report_statistics->addValue('review', 1);
+	}
+
 	// model/catalog/review/deleteReview/after
 	public function removeReview(&$route, &$args, &$output) {
 		$this->load->model('setting/statistics');
 
 		$this->model_report_statistics->removeValue('review', 1);
+	}
+
+	// model/sale/return/addReturn/after
+	public function addReturn(&$route, &$args, &$output) {
+		$this->load->model('report/statistics');
+
+		$this->model_report_statistics->addValue('return', 1);
 	}
 
 	// model/sale/return/deleteReturn/after
