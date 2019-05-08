8a61cb4bc7b6c56ddb3d7ce15eb0845616d2c757
v5manoel <manoel.vidal@opencartbrasil.com.br>
2019-05-08 04:27:52 -0300

    Melhorias na identificação de eventos estatísticos

--- admin/controller/event/statistics.php
+++ admin/controller/event/statistics.php
@@ -1,27 +1,27 @@
 <?php
 class ControllerEventStatistics extends Controller {
-	// model/catalog/review/addReview/after
+	// admin/model/catalog/review/addReview/after
 	public function addReview(&$route, &$args, &$output) {
 		$this->load->model('report/statistics');
 
 		$this->model_report_statistics->addValue('review', 1);
 	}
 
-	// model/catalog/review/deleteReview/before
+	// admin/model/catalog/review/deleteReview/before
 	public function deleteReview(&$route, &$args) {
 		$this->load->model('report/statistics');
 
 		$this->model_report_statistics->removeValue('review', 1);
 	}
 
-	// model/sale/return/addReturn/after
+	// admin/model/sale/return/addReturn/after
 	public function addReturn(&$route, &$args, &$output) {
 		$this->load->model('report/statistics');
 
 		$this->model_report_statistics->addValue('return', 1);
 	}
 
-	// model/sale/return/deleteReturn/before
+	// admin/model/sale/return/deleteReturn/before
 	public function deleteReturn(&$route, &$args) {
 		$this->load->model('report/statistics');
 
