875dc2e09edb218f52c32ad606152d3288f0b4b4
Manoel Vidal <dev@opencartbrasil.com.br>
2019-06-16 00:54:15 -0300

    Melhorias no layout do pedido para mobile na administração

--- catalog/controller/common/footer.php
+++ catalog/controller/common/footer.php
@@ -78,7 +78,7 @@ class ControllerCommonFooter extends Controller {
 		}
 
 		$data['scripts'] = $this->document->getScripts('footer');
-		
+
 		return $this->load->view('common/footer', $data);
 	}
 }
