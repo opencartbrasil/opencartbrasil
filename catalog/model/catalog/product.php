f992ab6c22f68a4fa0e1f9c18a1e8475a8ed502b
Manoel Vidal <dev@opencartbrasil.com.br>
2019-10-26 17:27:56 -0300

    Correção #31

--- catalog/model/catalog/product.php
+++ catalog/model/catalog/product.php
@@ -74,7 +74,7 @@ class ModelCatalogProduct extends Model {
 				$sql .= " LEFT JOIN `" . DB_PREFIX . "product` p ON (p2c.product_id = p.product_id)";
 			}
 		} else {
-			$sql .= " FROM `" . DB_PREFIX . "product p`";
+			$sql .= " FROM `" . DB_PREFIX . "product` p";
 		}
 
 		$sql .= " LEFT JOIN `" . DB_PREFIX . "product_description` pd ON (p.product_id = pd.product_id) LEFT JOIN `" . DB_PREFIX . "product_to_store` p2s ON (p.product_id = p2s.product_id) WHERE pd.language_id = '" . (int)$this->config->get('config_language_id') . "' AND p.status = '1' AND p.date_available <= NOW() AND p2s.store_id = '" . (int)$this->config->get('config_store_id') . "'";
