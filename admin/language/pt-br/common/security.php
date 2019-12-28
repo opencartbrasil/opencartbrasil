d2d28774ec11d5fbc30bd006df7c6051012ac192
Manoel Vidal <dev@opencartbrasil.com.br>
2019-12-28 19:12:19 -0300

    Correção no layout e tradução do procedimento de segurança

--- admin/language/pt-br/common/security.php
+++ admin/language/pt-br/common/security.php
@@ -5,22 +5,22 @@ $_['heading_title']    = 'Procedimento de segurança';
 // Text
 $_['text_success']     = 'Você moveu a pasta storage com sucesso!';
 $_['text_admin']       = 'Edite o arquivo admin/config.php e modifique';
-$_['text_security']    = 'É muito importante que você mova a pasta storage para fora do diretório público (public_html, www ou htdocs por exemplo), de modo que ela não seja acessível pela web.';
-$_['text_choose']      = 'Escolha como deseja mover o diretório de armazenamento';
-$_['text_automatic']   = 'Mover automaticamente';
-$_['text_manual']      = 'Mover manualmente';
-$_['text_move']        = 'Mover';
-$_['text_to']          = 'para';
-$_['text_config']      = 'Edite o arquivo config.php e modifique';
-$_['text_admin']       = 'Edite o arquivo admin/config.php e modifique';
+$_['text_security']    = 'É muito importante modificar o local da pasta storage para fora do diretório público (public_html, www ou htdocs por exemplo), de modo que ela não seja acessível pela web.';
+$_['text_choose']      = 'Selecione como deseja modificar o local da pasta storage';
+$_['text_automatic']   = 'Modificar automaticamente';
+$_['text_manual']      = 'Modificar manualmente';
+$_['text_move']        = 'Modifique a pasta:';
+$_['text_to']          = 'Para:';
+$_['text_config']      = 'Edite o arquivo config.php e modifique a linha abaixo:';
+$_['text_admin']       = 'Edite o arquivo admin/config.php e modifique a linha abaixo:';
 
 // Button
-$_['button_move']      = 'Mover';
+$_['button_move']      = 'Modificar';
 $_['button_manual']    = 'Manual';
 
 // Error
-$_['error_permission'] = 'Atenção: Você não tem permissão para mover a pasta storage!';
-$_['error_path']       = 'Atenção: Caminho inválido!';
-$_['error_directory']  = 'Atenção: Diretório inválido!';
-$_['error_exists']     = 'Atenção: O diretório já existe!';
-$_['error_writable']   = 'Atenção: Os arquivos config.php e admin/config.php não tem permissão de escrita!';
+$_['error_permission'] = 'Atenção: Você não tem permissão para modificar o local da pasta storage!';
+$_['error_path']       = 'Atenção: Caminho inválido.';
+$_['error_directory']  = 'Atenção: Diretório inválido.';
+$_['error_exists']     = 'Atenção: O diretório já existe.';
+$_['error_writable']   = 'Atenção: Os arquivos config.php e admin/config.php não tem permissão de escrita.';
