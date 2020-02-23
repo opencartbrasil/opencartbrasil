81bfaf91c4128a6e0e4c1cdbb2d946842334aa63
Manoel Vidal <dev@opencartbrasil.com.br>
2020-02-23 07:26:44 -0300

    Melhorias no gerenciamento de sessões

--- admin/model/cron/session.php
+++ admin/model/cron/session.php
@@ -1,6 +1,6 @@
 <?php
 class ModelCronSession extends Model {
 	public function deleteExpires() {
-		$this->db->query("DELETE FROM `" . DB_PREFIX . "session` WHERE expire < '" . $this->db->escape(date('Y-m-d H:i:s', time())) . "'");
+		$this->db->query("DELETE FROM `" . DB_PREFIX . "session` WHERE expire < NOW()'");
 	}
 }
