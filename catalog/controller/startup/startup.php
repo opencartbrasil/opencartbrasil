f177af8308673daf7c9da4ef07309899bbdea85f
xxvirusxx <condor20_05@yahoo.it>
2020-05-23 12:23:59 +0300

    Fix customer login when no address
    #43

--- catalog/controller/startup/startup.php
+++ catalog/controller/startup/startup.php
@@ -155,7 +155,7 @@ class ControllerStartupStartup extends Controller {
 		// Tax
 		$this->registry->set('tax', new Cart\Tax($this->registry));
 
-		if (isset($this->session->data['shipping_address'])) {
+		if (isset($this->session->data['shipping_address']['country_id']) && isset($this->session->data['shipping_address']['zone_id'])) {
 			$this->tax->setShippingAddress($this->session->data['shipping_address']['country_id'], $this->session->data['shipping_address']['zone_id']);
 		} elseif ($this->config->get('config_tax_default') == 'shipping') {
 			$this->tax->setShippingAddress($this->config->get('config_country_id'), $this->config->get('config_zone_id'));
