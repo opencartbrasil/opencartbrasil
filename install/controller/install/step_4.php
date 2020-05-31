bcf36fe332f2fb2a54e246aed54ad91f4b595503
Manoel Vidal <dev@opencartbrasil.com.br>
2020-05-31 01:42:45 -0300

    Melhorias e correções na instalação do OpenCart

--- install/controller/install/step_4.php
+++ install/controller/install/step_4.php
@@ -13,10 +13,6 @@ class ControllerInstallStep4 extends Controller {
 			$data['success'] = '';
 		}
 
-		$data['maxmind'] = $this->url->link('3rd_party/maxmind');
-		$data['openbay'] = $this->url->link('3rd_party/openbay');
-		$data['extension'] = $this->url->link('3rd_party/extension');
-
 		$data['footer'] = $this->load->controller('common/footer');
 		$data['header'] = $this->load->controller('common/header');
 
