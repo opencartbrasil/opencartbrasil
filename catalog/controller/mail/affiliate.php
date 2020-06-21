58e85496a1d1b6450f51b39e77106948a81c51e5
Manoel Vidal <dev@opencartbrasil.com.br>
2020-06-21 13:29:16 -0300

    Correção no envio do e-mail de notificação para novo afiliado

--- catalog/controller/mail/affiliate.php
+++ catalog/controller/mail/affiliate.php
@@ -129,10 +129,10 @@ class ControllerMailAffiliate extends Controller {
 			$mail->send();
 
 			// Send to additional alert emails if new affiliate email is enabled
-			$emails = explode(',', (array)$this->config->get('config_mail_alert_email'));
+			$emails = explode(',', $this->config->get('config_mail_alert_email'));
 
 			foreach ($emails as $email) {
-				if (utf8_strlen($email) > 0 && filter_var($email, FILTER_VALIDATE_EMAIL)) {
+				if ($email && filter_var($email, FILTER_VALIDATE_EMAIL)) {
 					$mail->setTo($email);
 					$mail->send();
 				}
