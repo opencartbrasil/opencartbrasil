b7047bf57d34ee8e6afb55d034072bf1e62fcd79
v5mvidal <dev@opencartbrasil.com.br>
2020-10-10 13:23:52 -0300

    fix: correção na tradução do voucher na administração

--- admin/language/pt-br/mail/voucher.php
+++ admin/language/pt-br/mail/voucher.php
@@ -7,3 +7,4 @@ $_['text_from']     = 'Quem lhe enviou este vale presentes foi %s';
 $_['text_message']  = 'O vale presentes veio com a seguinte mensagem:';
 $_['text_redeem']   = 'Para utilizar este vale presentes, anote o código <b>%s</b>, depois acesse o link abaixo e escolha os produtos que você deseja comprar. Você deve inserir o código do vale presentes na página do carrinho de compras, antes de finalizar o pedido.';
 $_['text_footer']   = 'Caso tenha alguma dúvida, responda este e-mail.';
+$_['text_sent']     = 'O vale presente foi enviado!';
