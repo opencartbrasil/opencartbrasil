73552263a75fe50d38989acf9c8aed2e5b3b2c74
Manoel Vidal <manoel.vidal@opencartbrasil.com.br>
2020-10-10 02:30:26 -0300

    feat: remoção da extensão frenet

--- catalog/language/en-gb/extension/shipping/frenet.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Text
-$_['text_title']      = 'Frenet';
-$_['text_sucesso']    = 'Reconexão realizada com sucesso!';
-$_['text_free']       = '<span style="color:green;font-weight:bold">Grátis</span>';
-
-// Error
-$_['error_conexao']   = 'Não foi possível estabelecer conexão com os Frenet. Tentando reconectar...';
-$_['error_reconexao'] = 'Falha na tentativa de reconectar com os Frenet. O WebService dos Frenet apresenta instabilidade ou está fora do ar.';
