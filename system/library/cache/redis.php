04ae9a38e6612e1946c532463c7c66a3f6571fcc
v5mvidal <dev@opencartbrasil.com.br>
2020-10-10 19:27:11 -0300

    refactor: cache redis

--- system/library/cache/redis.php
+++ system/library/cache/redis.php
@@ -2,32 +2,32 @@
 namespace Cache;
 class Redis {
 	private $expire;
-	private $cache;
+	private $redis;
 
 	public function __construct($expire) {
 		$this->expire = $expire;
 
-		$this->cache = new \Redis();
-		$this->cache->pconnect(CACHE_HOSTNAME, CACHE_PORT);
+		$this->redis = new \Redis();
+		$this->redis->pconnect(CACHE_HOSTNAME, CACHE_PORT);
 	}
 
 	public function get($key) {
-		$data = $this->cache->get(CACHE_PREFIX . $key);
+		$data = $this->redis->get(CACHE_PREFIX . $key);
 
 		return json_decode($data, true);
 	}
 
 	public function set($key, $value) {
-		$status = $this->cache->set(CACHE_PREFIX . $key, json_encode($value));
+		$status = $this->redis->set(CACHE_PREFIX . $key, json_encode($value));
 
 		if ($status) {
-			$this->cache->setTimeout(CACHE_PREFIX . $key, $this->expire);
+			$this->redis->expire(CACHE_PREFIX . $key, $this->expire);
 		}
 
 		return $status;
 	}
 
 	public function delete($key) {
-		$this->cache->delete(CACHE_PREFIX . $key);
+		$this->redis->del(CACHE_PREFIX . $key);
 	}
 }
