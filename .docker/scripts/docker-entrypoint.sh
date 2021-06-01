f15aa92aae566e98ea7e16cfe7554046b7fc81fd
Valdeir S <valdeir@v5market.com>
2021-06-01 17:52:35 -0300

    refactor: adiciona validaçoes no docker-entrypoint
    Caso as variáveis de ambientes não sejam definidas,
    ignora a instalação automática.
    
    Signed-off-by: Valdeir S <valdeir@v5market.com>

--- .docker/scripts/docker-entrypoint.sh
+++ .docker/scripts/docker-entrypoint.sh
@@ -1,35 +1,51 @@
 #!/usr/bin/env bash
 
-if [ ! -f config.php ] || [ ! -s config.php ]; then
-    until nc -z -w30 $DB_HOSTNAME ${DB_PORT:-3306}; do
-        echo "Aguardando inicialização do banco de dados"
-        sleep 5
-    done
+cli_arguments=(
+  "${DB_HOSTNAME:-0}"
+  "${DB_PASSWORD:-0}"
+  "${HTTP_SERVER:-0}"
+)
 
-    password_default=$(cat /dev/urandom | tr -cd A-Za-z0-9 | head -c 10)
+is_valid=1
 
-    cli_arguments=(
-        --db_driver "pdo" \
-        --db_hostname "${DB_HOSTNAME}" \
-        --db_username "${DB_USERNAME:-root}" \
-        --db_password "${DB_PASSWORD}" \
-        --db_database "${DB_DATABASE:-opencartbrasil}" \
-        --db_port "${DB_PORT:-3306}" \
-        --db_prefix "${DB_PREFIX:-ocbr_}" \
-        --username "${USERNAME:-admin}" \
-        --password "${PASSWORD:-$password_default}" \
-        --email "${EMAIL:-web@master}" \
-        --http_server "${HTTP_SERVER%/}/"
-    )
+for cli_arg in ${cli_arguments[@]}; do
+  if [ $cli_arg -eq 0 ]; then
+    is_valid=0
+  fi
+done
 
-    php install/cli_install.php install ${cli_arguments[@]};
+if [ $is_valid -eq 1 ]; then
+  if [ ! -f config.php ] || [ ! -s config.php ]; then
+      until nc -z -w30 $DB_HOSTNAME ${DB_PORT:-3306}; do
+          echo "Aguardando inicialização do banco de dados"
+          sleep 5
+      done
 
-    if [ -z $PASSWORD ]; then
-        echo -e "\nCredenciais de acesso"
-        echo "Usuário: ${USERNAME:-'admin'}"
-        echo "Senha: ${PASSWORD:-$password_default}"
-        echo -e "Após logar, troque os dados para sua segurança\n\n\n"
-    fi
+      password_default=$(cat /dev/urandom | tr -cd A-Za-z0-9 | head -c 10)
+
+      cli_arguments=(
+          --db_driver "pdo" \
+          --db_hostname "${DB_HOSTNAME}" \
+          --db_username "${DB_USERNAME:-root}" \
+          --db_password "${DB_PASSWORD}" \
+          --db_database "${DB_DATABASE:-opencartbrasil}" \
+          --db_port "${DB_PORT:-3306}" \
+          --db_prefix "${DB_PREFIX:-ocbr_}" \
+          --username "${USERNAME:-admin}" \
+          --password "${PASSWORD:-$password_default}" \
+          --email "${EMAIL:-web@master}" \
+          --http_server "${HTTP_SERVER%/}/"
+      )
+
+      php install/cli_install.php install ${cli_arguments[@]};
+
+      if [ -z $PASSWORD ]; then
+          echo -e "\nCredenciais de acesso"
+          echo "Usuário: ${USERNAME:-'admin'}"
+          echo "Senha: ${PASSWORD:-$password_default}"
+          echo -e "Após logar, troque os dados para sua segurança\n\n\n"
+      fi
+  fi
 fi
 
 folders=(
