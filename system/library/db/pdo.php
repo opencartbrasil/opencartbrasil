c242947078d1a40218c114b431629d924adcf26f
v5mvidal <dev@opencartbrasil.com.br>
2021-06-01 17:48:32 -0300

    refactor: .editorconfig

--- system/library/db/pdo.php
+++ system/library/db/pdo.php
@@ -1,79 +1,79 @@
 <?php
 namespace DB;
 final class PDO {
-    private $connection;
+	private $connection;
 
-    public function __construct($hostname, $username, $password, $database, $port = '3306') {
-        try {
-            $pdo = @new \PDO("mysql:host=" . $hostname . ";port=" . $port . ";dbname=" . $database . ";charset=UTF8", $username, $password, array(\PDO::ATTR_PERSISTENT => false));
-        } catch (\PDOException $e) {
-            $message = $e->getMessage();
-            $message = str_replace(array($hostname, $username, $password, $database, $port), '*********', $message);
+	public function __construct($hostname, $username, $password, $database, $port = '3306') {
+		try {
+			$pdo = @new \PDO("mysql:host=" . $hostname . ";port=" . $port . ";dbname=" . $database . ";charset=UTF8", $username, $password, array(\PDO::ATTR_PERSISTENT => false));
+		} catch (\PDOException $e) {
+			$message = $e->getMessage();
+			$message = str_replace(array($hostname, $username, $password, $database, $port), '*********', $message);
 
-            throw new \Exception($message, $e->getCode());
-        }
+			throw new \Exception($message, $e->getCode());
+		}
 
-        if ($pdo) {
-            $this->connection = $pdo;
-            $this->connection->query("SET SESSION sql_mode = 'NO_ZERO_IN_DATE,NO_ZERO_DATE,NO_ENGINE_SUBSTITUTION'");
-        }
-    }
+		if ($pdo) {
+			$this->connection = $pdo;
+			$this->connection->query("SET SESSION sql_mode = 'NO_ZERO_IN_DATE,NO_ZERO_DATE,NO_ENGINE_SUBSTITUTION'");
+		}
+	}
 
-    public function query($sql, $params = array()) {
-        $this->statement = $this->connection->prepare($sql);
+	public function query($sql, $params = array()) {
+		$this->statement = $this->connection->prepare($sql);
 
-        try {
-            if ($this->statement && $this->statement->execute($params)) {
-                $data = array();
+		try {
+			if ($this->statement && $this->statement->execute($params)) {
+				$data = array();
 
-                while ($row = $this->statement->fetch(\PDO::FETCH_ASSOC)) {
-                    $data[] = $row;
-                }
+				while ($row = $this->statement->fetch(\PDO::FETCH_ASSOC)) {
+					$data[] = $row;
+				}
 
-                $result = new \stdClass();
-                $result->row = (isset($data[0]) ? $data[0] : array());
-                $result->rows = $data;
-                $result->num_rows = $this->statement->rowCount();
-            }
-        } catch (\PDOException $e) {
-            throw new \Exception('Erro: ' . $e->getMessage() . '<br>Código : ' . $e->getCode() . '<br>' . $sql);
-        }
+				$result = new \stdClass();
+				$result->row = (isset($data[0]) ? $data[0] : array());
+				$result->rows = $data;
+				$result->num_rows = $this->statement->rowCount();
+			}
+		} catch (\PDOException $e) {
+			throw new \Exception('Erro: ' . $e->getMessage() . '<br>Código : ' . $e->getCode() . '<br>' . $sql);
+		}
 
-        if (!isset($result)) {
-            $result = new \stdClass();
-            $result->num_rows = 0;
-            $result->row = array();
-            $result->rows = array();
-        }
+		if (!isset($result)) {
+			$result = new \stdClass();
+			$result->num_rows = 0;
+			$result->row = array();
+			$result->rows = array();
+		}
 
-        return $result;
-    }
+		return $result;
+	}
 
-    public function escape($value) {
-        return str_replace(array("\\", "\0", "\n", "\r", "\x1a", "'", '"'), array("\\\\", "\\0", "\\n", "\\r", "\Z", "\'", '\"'), $value);
-    }
+	public function escape($value) {
+		return str_replace(array("\\", "\0", "\n", "\r", "\x1a", "'", '"'), array("\\\\", "\\0", "\\n", "\\r", "\Z", "\'", '\"'), $value);
+	}
 
-    public function countAffected() {
-        if ($this->statement) {
-            return $this->statement->rowCount();
-        } else {
-            return 0;
-        }
-    }
+	public function countAffected() {
+		if ($this->statement) {
+			return $this->statement->rowCount();
+		} else {
+			return 0;
+		}
+	}
 
-    public function getLastId() {
-        return $this->connection->lastInsertId();
-    }
+	public function getLastId() {
+		return $this->connection->lastInsertId();
+	}
 
-    public function isConnected() {
-        if ($this->connection) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+	public function isConnected() {
+		if ($this->connection) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-    public function __destruct() {
-        unset($this->connection);
-    }
+	public function __destruct() {
+		unset($this->connection);
+	}
 }
--- system/library/db/pgsql.php
+++ system/library/db/pgsql.php
@@ -1,87 +1,87 @@
 <?php
 namespace DB;
 final class PgSQL {
-    private $connection;
+	private $connection;
 
-    public function __construct($hostname, $username, $password, $database, $port = '5432') {
-        try {
-            $pg = @pg_connect('hostname=' . $hostname . ' port=' . $port . ' username=' . $username . ' password=' . $password . ' database=' . $database);
-        } catch (\Exception $e) {
-            $message = $e->getMessage();
-            $message = str_replace(array($hostname, $username, $password, $database, $port), '*********', $message);
+	public function __construct($hostname, $username, $password, $database, $port = '5432') {
+		try {
+			$pg = @pg_connect('hostname=' . $hostname . ' port=' . $port . ' username=' . $username . ' password=' . $password . ' database=' . $database);
+		} catch (\Exception $e) {
+			$message = $e->getMessage();
+			$message = str_replace(array($hostname, $username, $password, $database, $port), '*********', $message);
 
-            throw new \Exception($message, $e->getCode());
-        }
+			throw new \Exception($message, $e->getCode());
+		}
 
-        if ($pg) {
-            $this->connection = $pg;
-            pg_query($this->connection, "SET CLIENT_ENCODING TO 'UTF8'");
-        }
-    }
+		if ($pg) {
+			$this->connection = $pg;
+			pg_query($this->connection, "SET CLIENT_ENCODING TO 'UTF8'");
+		}
+	}
 
-    public function query($sql) {
-        $resource = pg_query($this->connection, $sql);
+	public function query($sql) {
+		$resource = pg_query($this->connection, $sql);
 
-        if ($resource) {
-            if (is_resource($resource)) {
-                $i = 0;
+		if ($resource) {
+			if (is_resource($resource)) {
+				$i = 0;
 
-                $data = array();
+				$data = array();
 
-                while ($row = pg_fetch_assoc($resource)) {
-                    $data[$i] = $row;
+				while ($row = pg_fetch_assoc($resource)) {
+					$data[$i] = $row;
 
-                    $i++;
-                }
+					$i++;
+				}
 
-                pg_free_result($resource);
+				pg_free_result($resource);
 
-                $result = new \stdClass();
-                $result->num_rows = $i;
-                $result->row = isset($data[0]) ? $data[0] : array();
-                $result->rows = $data;
+				$result = new \stdClass();
+				$result->num_rows = $i;
+				$result->row = isset($data[0]) ? $data[0] : array();
+				$result->rows = $data;
 
-                unset($data);
-            }
-        } else {
-            throw new \Exception('Erro: ' . pg_result_error($this->connection) . '<br>Código: ' . $sql);
-        }
+				unset($data);
+			}
+		} else {
+			throw new \Exception('Erro: ' . pg_result_error($this->connection) . '<br>Código: ' . $sql);
+		}
 
-        if (!isset($result)) {
-            $result = new \stdClass();
-            $result->num_rows = 0;
-            $result->row = array();
-            $result->rows = array();
-        }
+		if (!isset($result)) {
+			$result = new \stdClass();
+			$result->num_rows = 0;
+			$result->row = array();
+			$result->rows = array();
+		}
 
-        return $result;
-    }
+		return $result;
+	}
 
-    public function escape($value) {
-        return pg_escape_string($this->connection, $value);
-    }
+	public function escape($value) {
+		return pg_escape_string($this->connection, $value);
+	}
 
-    public function countAffected() {
-        return pg_affected_rows($this->connection);
-    }
+	public function countAffected() {
+		return pg_affected_rows($this->connection);
+	}
 
-    public function isConnected() {
-        if (pg_connection_status($this->connection) == PGSQL_CONNECTION_OK) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+	public function isConnected() {
+		if (pg_connection_status($this->connection) == PGSQL_CONNECTION_OK) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-    public function getLastId() {
-        $query = $this->query("SELECT LASTVAL() AS `id`");
+	public function getLastId() {
+		$query = $this->query("SELECT LASTVAL() AS `id`");
 
-        return $query->row['id'];
-    }
+		return $query->row['id'];
+	}
 
-    public function __destruct() {
-        if ($this->isConnected()) {
-            pg_close($this->connection);
-        }
-    }
+	public function __destruct() {
+		if ($this->isConnected()) {
+			pg_close($this->connection);
+		}
+	}
 }
