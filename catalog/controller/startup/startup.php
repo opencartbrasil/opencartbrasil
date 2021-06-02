ef1faa2f06afa8de198fff8758d3a9b19fa4942c
v5mvidal <dev@opencartbrasil.com.br>
2021-06-02 19:41:53 -0300

    fix: adição do tracking no startup

--- catalog/controller/startup/startup.php
+++ catalog/controller/startup/startup.php
@@ -125,6 +125,13 @@ class ControllerStartupStartup extends Controller {
 			$this->config->set('config_customer_group_id', $this->config->get('config_customer_group_id'));
 		}
 
+		// Tracking Code
+		if (isset($this->request->get['tracking'])) {
+			setcookie('tracking', $this->request->get['tracking'], time() + 3600 * 24 * 1000, '/');
+
+			$this->db->query("UPDATE `" . DB_PREFIX . "marketing` SET clicks = (clicks + 1) WHERE code = '" . $this->db->escape($this->request->get['tracking']) . "'");
+		}
+
 		// Currency
 		$code = '';
 
