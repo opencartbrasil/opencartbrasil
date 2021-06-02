08b0c07ce5c651783e8ef06ecd2c64fdd09c4125
v5mvidal <dev@opencartbrasil.com.br>
2021-06-02 17:36:36 -0300

    fix: só exibe moedas habilitadas na loja

--- catalog/model/localisation/currency.php
+++ catalog/model/localisation/currency.php
@@ -12,7 +12,7 @@ class ModelLocalisationCurrency extends Model {
 		if (!$currency_data) {
 			$currency_data = array();
 
-			$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "currency` ORDER BY title ASC");
+			$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "currency` WHERE `status` = '1' ORDER BY `title` ASC");
 
 			foreach ($query->rows as $result) {
 				$currency_data[$result['code']] = array(
