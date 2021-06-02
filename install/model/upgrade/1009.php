f4b8182cf7a2573120dba4e7a3d9b6aa6b3bdb8a
v5mvidal <dev@opencartbrasil.com.br>
2021-06-02 16:53:33 -0300

    fix: correção no upgrade do banco de dados

--- install/model/upgrade/1009.php
+++ install/model/upgrade/1009.php
@@ -67,10 +67,13 @@ class ModelUpgrade1009 extends Model {
 			$this->db->query("DROP TABLE `" . DB_PREFIX . "affiliate_transaction`");
 		}
 
+		// Api
 		$query = $this->db->query("SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '" . DB_DATABASE . "' AND TABLE_NAME = '" . DB_PREFIX . "api' AND COLUMN_NAME = 'name'");
 
 		if ($query->num_rows) {
-			$this->db->query("ALTER TABLE `" . DB_PREFIX . "api` CHANGE `name` `username` VARCHAR(64) NOT NULL");
+			$this->db->query("ALTER TABLE `" . DB_PREFIX . "api` DROP COLUMN `username`");
+			$this->db->query("ALTER TABLE `" . DB_PREFIX . "api` CHANGE COLUMN `name` `username` VARCHAR(64) NOT NULL");
+			$this->db->query("ALTER TABLE `" . DB_PREFIX . "api` MODIFY `username` VARCHAR(64) NOT NULL AFTER `api_id`");
 		}
 
 		// Events
@@ -125,7 +128,7 @@ class ModelUpgrade1009 extends Model {
 		foreach ($files as $file) {
 			$lines = file($file);
 
-			for ($i = 0; $i < count($lines); $i++) { 
+			for ($i = 0; $i < count($lines); $i++) {
 				if ((strpos($lines[$i], 'DIR_IMAGE') !== false) && (strpos($lines[$i + 1], 'DIR_STORAGE') === false)) {
 					array_splice($lines, $i + 1, 0, array('define(\'DIR_STORAGE\', DIR_SYSTEM . \'storage/\');'));
 				}
@@ -149,7 +152,7 @@ class ModelUpgrade1009 extends Model {
 				if (strpos($lines[$i], 'DIR_MODIFICATION') !== false) {
 					$lines[$i] = 'define(\'DIR_MODIFICATION\', DIR_STORAGE . \'modification/\');' . "\n";
 				}
-				
+
 				if (strpos($lines[$i], 'DIR_SESSION') !== false) {
 					$lines[$i] = 'define(\'DIR_SESSION\', DIR_STORAGE . \'session/\');' . "\n";
 				}
