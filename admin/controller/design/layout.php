2ba6d600d23339962312ef0bdb7132b699c1a171
v5mvidal <dev@opencartbrasil.com.br>
2021-06-02 11:58:18 -0300

    fix: remoção de variável na utilizada no cadastro de layouts

--- admin/controller/design/layout.php
+++ admin/controller/design/layout.php
@@ -366,11 +366,8 @@ class ControllerDesignLayout extends Controller {
 		foreach ($layout_modules as $layout_module) {
 			$part = explode('.', $layout_module['code']);
 
-			$this->load->language('extension/module/' . $part[0]);
-
 			if (!isset($part[1])) {
 				$data['layout_modules'][] = array(
-					'name'       => strip_tags($this->language->get('heading_title')),
 					'code'       => $layout_module['code'],
 					'edit'       => $this->url->link('extension/module/' . $part[0], 'user_token=' . $this->session->data['user_token'], true),
 					'position'   => $layout_module['position'],
@@ -378,10 +375,9 @@ class ControllerDesignLayout extends Controller {
 				);
 			} else {
 				$module_info = $this->model_setting_module->getModule($part[1]);
-				
+
 				if ($module_info) {
 					$data['layout_modules'][] = array(
-						'name'       => strip_tags($module_info['name']),
 						'code'       => $layout_module['code'],
 						'edit'       => $this->url->link('extension/module/' . $part[0], 'user_token=' . $this->session->data['user_token'] . '&module_id=' . $part[1], true),
 						'position'   => $layout_module['position'],
