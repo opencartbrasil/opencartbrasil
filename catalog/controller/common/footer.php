c34ae45ab968a1691c56dca0616f0c2ef931e11c
v5mvidal <dev@opencartbrasil.com.br>
2021-06-02 17:58:44 -0300

    refactor: suporte para styles no footer

--- catalog/controller/common/footer.php
+++ catalog/controller/common/footer.php
@@ -78,6 +78,7 @@ class ControllerCommonFooter extends Controller {
 		}
 
 		$data['scripts'] = $this->document->getScripts('footer');
+		$data['styles'] = $this->document->getStyles('footer');
 
 		return $this->load->view('common/footer', $data);
 	}
