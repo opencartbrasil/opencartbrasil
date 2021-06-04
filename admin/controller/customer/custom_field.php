6da96d260c4ffcda13fd10f6b72946bb4a900a95
Valdeir S <valdeir@v5market.com>
2021-06-04 18:16:15 -0300

    fix: corrige falha no cadastro do campo personalizado
    Antes, ao editar o campo personalizado, a caixa de seleção "Obrigatório para"
    era marcada, mesmo não sendo obrigatório o preenchimento
    
    Signed-off-by: Valdeir S <valdeir@v5market.com>

--- admin/controller/customer/custom_field.php
+++ admin/controller/customer/custom_field.php
@@ -446,7 +446,7 @@ class ControllerCustomerCustomField extends Controller {
 		$data['custom_field_required'] = array();
 
 		foreach ($custom_field_customer_groups as $custom_field_customer_group) {
-			if(isset($custom_field_customer_group['required']) && isset($custom_field_customer_group['customer_group_id'])) {
+			if(isset($custom_field_customer_group['required']) && isset($custom_field_customer_group['customer_group_id']) && $custom_field_customer_group['required']) {
 				$data['custom_field_required'][] = $custom_field_customer_group['customer_group_id'];
 			} else {
 				$data['custom_field_required'][] = '';
