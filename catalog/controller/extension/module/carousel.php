5331fa708559660dace1fcdfb04d33ac0e97c774
v5mvidal <manoel@v5market.com>
2021-06-18 23:22:10 -0300

    refactor: utilização do arquivo swiper.jquery.min.js no lugar do swiper.jquery.js

--- catalog/controller/extension/module/carousel.php
+++ catalog/controller/extension/module/carousel.php
@@ -5,10 +5,10 @@ class ControllerExtensionModuleCarousel extends Controller {
 
 		$this->load->model('design/banner');
 		$this->load->model('tool/image');
-		
+
 		$this->document->addStyle('catalog/view/javascript/jquery/swiper/css/swiper.min.css');
 		$this->document->addStyle('catalog/view/javascript/jquery/swiper/css/opencart.css');
-		$this->document->addScript('catalog/view/javascript/jquery/swiper/js/swiper.jquery.js');
+		$this->document->addScript('catalog/view/javascript/jquery/swiper/js/swiper.jquery.min.js');
 
 		$data['banners'] = array();
 
