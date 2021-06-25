9146c9b8dc72ef033fab5be057f65fa291a7ed38
v5mvidal <manoel@v5market.com>
2021-06-25 11:41:57 -0300

    feat: versão 1.6.0-rc.1

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 // Version
 define('VERSION', '3.0.3.3');
-define('OPENCART_BRASIL', '1.6-beta.3');
+define('OPENCART_BRASIL', '1.6.0-rc.1');
 
 // Configuration
 if (is_file('config.php')) {
