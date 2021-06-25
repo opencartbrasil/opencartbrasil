fc329a7583d4fa429bfca599270f59a002d99419
Valdeir S <valdeir@v5market.com>
2021-06-25 18:08:41 -0300

    feat: adiciona middlewares para log
    Os logs neste middleware serão registrados antes e após a executação
    do *controller* da rota.
    
    Ele capturará e armazenará os dados utilizando o sistema de log nativo
    do OpenCart
    
    Signed-off-by: Valdeir S <valdeir@v5market.com>

---
+++ api/controller/middlewares/logs.php
@@ -0,0 +1,64 @@
+<?php
+
+class ControllerMiddlewaresLogs extends Controller {
+
+	/** @var string Identificador da requisição */
+	private static $request_id;
+
+	public function getRequestId() {
+		return self::$request_id;
+	}
+
+	public function before() {
+		$logs = array();
+
+		if ($this->jwt) {
+			$logs = array_merge($logs, array(
+				'iss' => $this->jwt->iss,
+				'application_name' => $this->jwt->application_name,
+			));
+
+			self::$request_id = hash('sha256', $this->jwt->application_name . hrtime(true) . uniqid());
+		} else {
+			self::$request_id = hash('sha256', hrtime(true) . uniqid());
+		}
+
+		$logs = array_merge($logs, array(
+			'request_id' => self::$request_id,
+			'user_agent' => $this->request->server['HTTP_USER_AGENT'],
+			'timestamp' => microtime(true),
+			'route' => $this->request->get['route'],
+			'URL' => $this->request->server['REQUEST_URI'],
+			'query_strings' => $this->request->get,
+			'request_headers' => $this->request->headers,
+			'request_body' => $this->request->json,
+			'ip_address' => $this->request->server['REMOTE_ADDR'],
+		));
+
+		$this->log->write($logs);
+	}
+
+	public function after() {
+		$logs = array(
+			'request_id' => self::$request_id,
+		);
+
+		if ($this->jwt) {
+			$logs = array_merge($logs, array(
+				'iss' => $this->jwt->iss,
+				'application_name' => $this->jwt->application_name,
+			));
+		}
+
+		$logs = array_merge($logs, array(
+			'user_agent' => $this->request->server['HTTP_USER_AGENT'],
+			'ip_address' => $this->request->server['REMOTE_ADDR'],
+			'timestamp' => microtime(true),
+			'route' => $this->request->get['route'],
+			'response_headers' => $this->response->getHeaders(),
+			'response_body' => $this->response->getOutput(),
+		));
+
+		$this->log->write($logs);
+	}
+}
