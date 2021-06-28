b471ec0e50fa744fe35a474bf007cb90d664834d
Valdeir S <valdeir@v5market.com>
2021-06-28 16:58:21 -0300

    refactor: passa a utilizar microtime para calcular tempo da requisição
    Signed-off-by: Valdeir S <valdeir@v5market.com>

--- api/controller/middlewares/response_time.php
+++ api/controller/middlewares/response_time.php
@@ -2,10 +2,10 @@
 
 class ControllerMiddlewaresResponseTime extends Controller {
 	public function before() {
-		$this->response->addHeader('X-Request-Time-Start: ' . time());
+		$this->response->addHeader('X-Request-Time-Start: ' . microtime(true));
 	}
 
 	public function after() {
-		$this->response->addHeader('X-Request-Time-End: ' . time());
+		$this->response->addHeader('X-Request-Time-End: ' . microtime(true));
 	}
 }
