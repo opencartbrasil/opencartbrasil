84c94ac63500f31d35838fcdcce7a339b4b8e9fc
Valdeir S <valdeir@v5market.com>
2021-07-07 15:27:26 -0300

    feat: adiciona suporte para remoção de produtos na API
    Signed-off-by: Valdeir S <valdeir@v5market.com>

--- api/model/catalog/product.php
+++ api/model/catalog/product.php
@@ -298,6 +298,31 @@ class ModelCatalogProduct extends Model {
 		return $query->rows;
 	}
 
+	public function deleteProduct(int $product_id) {
+		$this->db->query('DELETE FROM `' . DB_PREFIX . 'product` WHERE `product_id` = "' . $product_id . '"');
+		$this->db->query('DELETE FROM `' . DB_PREFIX . 'product_attribute` WHERE `product_id` = "' . $product_id . '"');
+		$this->db->query('DELETE FROM `' . DB_PREFIX . 'product_description` WHERE `product_id` = "' . $product_id . '"');
+		$this->db->query('DELETE FROM `' . DB_PREFIX . 'product_discount` WHERE `product_id` = "' . $product_id . '"');
+		$this->db->query('DELETE FROM `' . DB_PREFIX . 'product_filter` WHERE `product_id` = "' . $product_id . '"');
+		$this->db->query('DELETE FROM `' . DB_PREFIX . 'product_image` WHERE `product_id` = "' . $product_id . '"');
+		$this->db->query('DELETE FROM `' . DB_PREFIX . 'product_option` WHERE `product_id` = "' . $product_id . '"');
+		$this->db->query('DELETE FROM `' . DB_PREFIX . 'product_option_value` WHERE `product_id` = "' . $product_id . '"');
+		$this->db->query('DELETE FROM `' . DB_PREFIX . 'product_related` WHERE `product_id` = "' . $product_id . '"');
+		$this->db->query('DELETE FROM `' . DB_PREFIX . 'product_related` WHERE `related_id` = "' . $product_id . '"');
+		$this->db->query('DELETE FROM `' . DB_PREFIX . 'product_reward` WHERE `product_id` = "' . $product_id . '"');
+		$this->db->query('DELETE FROM `' . DB_PREFIX . 'product_special` WHERE `product_id` = "' . $product_id . '"');
+		$this->db->query('DELETE FROM `' . DB_PREFIX . 'product_to_category` WHERE `product_id` = "' . $product_id . '"');
+		$this->db->query('DELETE FROM `' . DB_PREFIX . 'product_to_download` WHERE `product_id` = "' . $product_id . '"');
+		$this->db->query('DELETE FROM `' . DB_PREFIX . 'product_to_layout` WHERE `product_id` = "' . $product_id . '"');
+		$this->db->query('DELETE FROM `' . DB_PREFIX . 'product_to_store` WHERE `product_id` = "' . $product_id . '"');
+		$this->db->query('DELETE FROM `' . DB_PREFIX . 'product_recurring` WHERE `product_id` = "'. $product_id . '"');
+		$this->db->query('DELETE FROM `' . DB_PREFIX . 'review` WHERE `product_id` = "' . $product_id . '"');
+		$this->db->query('DELETE FROM `' . DB_PREFIX . 'seo_url` WHERE `query` = "product_id="' . $product_id . '"');
+		$this->db->query('DELETE FROM `' . DB_PREFIX . 'coupon_product` WHERE `product_id` = "' . $product_id . '"');
+
+		$this->cache->delete('product');
+	}
+
 	/**
 	 * Verifica se um ou mais produto existe, através do ID
 	 *
@@ -645,5 +670,7 @@ class ModelCatalogProduct extends Model {
 				}
 			}
 		}
+
+		$this->cache->delete('product');
 	}
 }
