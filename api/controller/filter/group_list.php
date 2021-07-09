c1df2c70d1b7b5cb8481bf18974016975b3243c6
Valdeir S <valdeir@v5market.com>
2021-07-09 19:08:54 -0300

    feat: adiciona suporte para listar grupos de filtros e os filtros
    Signed-off-by: Valdeir S <valdeir@v5market.com>

---
+++ api/controller/filter/group_list.php
@@ -0,0 +1,92 @@
+<?php
+
+class ControllerFilterGroupList extends Controller {
+
+	private const HTTP_STATUS_400 = 400;
+
+	public function index() {
+		$this->load->model('catalog/filter');
+
+		/**
+		 * Page
+		 */
+		if (isset($this->request->get['page'])) {
+			$page = intval($this->request->get['page']);
+			$page = max($page, 1);
+		} else {
+			$page = 1;
+		}
+
+		/**
+		 * Items per page
+		 */
+		if (isset($this->request->get['per_page'])) {
+			$per_page = intval($this->request->get['per_page']);
+			$per_page = min($this->config->get('db_list_per_page'), $per_page);
+		} else {
+			$per_page = $this->config->get('db_list_per_page');
+		}
+
+		$filter_data = array(
+			'offset' => ($page - 1) * $per_page,
+			'limit' => $per_page
+		);
+
+		$filters = $this->model_catalog_filter->getFilterGroups($filter_data);
+
+		$filter_total_count = $this->model_catalog_filter->getTotalFilters();
+
+		$result_items = array();
+
+		foreach ($filters as $key => $filter_info) {
+			$filter_description = $this->model_catalog_filter->getFilterGroupDescriptionById($filter_info['filter_group_id']);
+			$filters = $this->model_catalog_filter->getFiltersByGroupId($filter_info['filter_group_id']);
+
+			$item = array(
+				'filter_group_id' => intval($filter_info['filter_group_id']),
+				'sort_order' => intval($filter_info['sort_order']),
+				'filters' => array_values($filters)
+			);
+
+			$result_items[] = array_merge($item, $filter_description);
+		}
+
+		$prev_page = max(1, $page - 1);
+		$last_page = ceil($filter_total_count / $per_page);
+		$next_page = intval(min($page + 1, $last_page));
+		$links = '/filter_group?page=%d&per_page=%d';
+
+		$result = array(
+			'items' => array_values($result_items),
+			'_metadata' => array(
+				'page' => $page,
+				'per_page' => $per_page,
+				'page_count' => count($result_items),
+				'total_count' => $filter_total_count,
+				'links' => array(
+					'self' => sprintf($links, $page, $per_page),
+					'first' => sprintf($links, 1, $per_page),
+					'previous' => ($page > 1) ? sprintf($links, $prev_page, $per_page) : null,
+					'next' => ($next_page !== $page) ? sprintf($links, $next_page, $per_page) : null,
+					'last' => sprintf($links, $last_page, $per_page)
+				)
+			)
+		);
+
+		$this->response->addHeader("X-Total-Count: $filter_total_count");
+		$this->response($result);
+	}
+
+	/**
+	 * Exibe resposta para o cliente
+	 *
+	 * @param int $status
+	 *
+	 * @return void
+	 */
+	protected function response(array $data = array(), int $status = 200) {
+		$this->response->addHeader('HTTP/1.1 ' . $status);
+		$this->response->addHeader('Content-Type: application/json');
+		$this->response->setOutput(json_encode($data));
+	}
+}
