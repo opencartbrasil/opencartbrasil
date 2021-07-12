4a417f8b33728b4d5c969b2d15aacb645f3f8df9
Valdeir S <valdeir@v5market.com>
2021-07-12 16:00:18 -0300

    feat: adiciona suporte para lista de fabricantes na API
    Signed-off-by: Valdeir S <valdeir@v5market.com>

--- api/model/catalog/manufacturer.php
+++ api/model/catalog/manufacturer.php
@@ -15,4 +15,57 @@ class ModelCatalogManufacturer extends Model {
 
 		return $result;
 	}
+
+	public function getManufacturers($data = array()) {
+		$sql = "SELECT * FROM " . DB_PREFIX . "manufacturer";
+
+		if (!empty($data['filter_name'])) {
+			$sql .= " WHERE name LIKE '%" . $this->db->escape($data['filter_name']) . "%'";
+		}
+
+		$sort_data = array(
+			'name',
+			'sort_order'
+		);
+
+		if (isset($data['sort']) && in_array($data['sort'], $sort_data)) {
+			$sql .= " ORDER BY " . $data['sort'];
+		} else {
+			$sql .= " ORDER BY name";
+		}
+
+		if (isset($data['order']) && ($data['order'] == 'DESC')) {
+			$sql .= " DESC";
+		} else {
+			$sql .= " ASC";
+		}
+
+		if (isset($data['offset']) || isset($data['limit'])) {
+			if ($data['offset'] < 0) {
+				$data['offset'] = 0;
+			}
+
+			if ($data['limit'] < 1) {
+				$data['limit'] = 20;
+			}
+
+			$sql .= " LIMIT " . (int)$data['offset'] . "," . (int)$data['limit'];
+		}
+
+		$query = $this->db->query($sql);
+
+		return $query->rows;
+	}
+
+	public function getTotalManufacturers($data = array()) {
+		$sql = "SELECT COUNT(*) AS total FROM " . DB_PREFIX . "manufacturer";
+
+		if (!empty($data['filter_name'])) {
+			$sql .= " WHERE name LIKE '%" . $this->db->escape($data['filter_name']) . "%'";
+		}
+
+		$query = $this->db->query($sql);
+
+		return $query->row['total'];
+	}
 }
