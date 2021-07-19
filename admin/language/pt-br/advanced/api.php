a7dc385093181194f8930842463410538f089d1d
Valdeir S <valdeir@v5market.com>
2021-07-19 18:26:14 -0300

    feat: adiciona base para gerenciamento das contas de API Rest
    Adiciona estrutura base para:
    
     - Atualizar
     - Criar
     - Deletar
     - Desabilitar
     - Listar
     - Ver logs
    
    Signed-off-by: Valdeir S <valdeir@v5market.com>

---
+++ admin/language/pt-br/advanced/api.php
@@ -0,0 +1,30 @@
+<?php
+
+$_['heading_title'] = 'API Rest';
+
+$_['column_description']   = 'Descrição';
+$_['column_ip_address']    = 'IP Address';
+$_['column_status'] 	   = 'Situação';
+$_['column_date_added']    = 'Cadastro';
+$_['column_date_modified'] = 'Modificação';
+$_['column_last_access']   = 'Último acesso';
+$_['column_action']        = 'Ação';
+
+$_['button_logs'] = 'Ver logs';
+$_['button_edit'] = 'Editar';
+$_['button_disable'] = 'Desabilitar';
+
+$_['entry_description'] = 'Descrição';
+$_['entry_permissions'] = 'Permissões';
+$_['entry_status'] = 'Situação';
+
+$_['text_success'] = 'API Rest modificada com sucesso!';
+$_['text_list'] = 'Listando API Rest';
+$_['text_form'] = 'Formulário';
+$_['text_general'] = 'Geral';
+$_['text_access'] = 'Acessos';
+$_['text_add_keys'] = 'Adicionar chaves';
+$_['text_edit_keys'] = 'Editar chaves';
+
+$_['error_permission'] = 'Atenção: Você não tem permissão para modicar API Rest!';
+$_['error_description'] = 'A descrição deve ter entre 3 e 255 caracteres!';
