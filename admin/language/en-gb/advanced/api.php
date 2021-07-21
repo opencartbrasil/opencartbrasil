e51243256e0ff15ca963fcc5756e257c31e4f021
Valdeir S <valdeir@v5market.com>
2021-07-21 16:59:45 -0300

    refactor: adiciona e formata arquivo de linguagem
    Adiciona o idioma inglês (en-us)
    
    Signed-off-by: Valdeir S <valdeir@v5market.com>

---
+++ admin/language/en-gb/advanced/api.php
@@ -0,0 +1,46 @@
+<?php
+// Heading
+$_['heading_title'] 	      = 'API Rest';
+
+// Column
+$_['column_description']      = 'Description';
+$_['column_ip_address']       = 'IP Address';
+$_['column_status'] 	      = 'Status';
+$_['column_date_added']       = 'Register';
+$_['column_date_modified']    = 'Modified';
+$_['column_last_access']      = 'Last Access';
+$_['column_type']   	      = 'Description';
+$_['column_action']           = 'Action';
+
+// Button
+$_['button_add']              = 'Generate new keys';
+$_['button_delete']           = 'Remove selected key';
+$_['button_cancel']           = 'Go back';
+$_['button_logs']             = 'View logs';
+$_['button_edit']             = 'Edit';
+$_['button_disable']          = 'Disable';
+
+// Entry
+$_['entry_description']       = 'Description';
+$_['entry_consumer_key']      = 'Consumer Key';
+$_['entry_consumer_secret']   = 'Consumer Secret';
+$_['entry_permissions']       = 'Permissions';
+$_['entry_status']            = 'Status';
+
+// Text
+$_['text_success']            = 'Rest API successfully modified!';
+$_['text_list']               = 'Client List';
+$_['text_form']               = 'Form';
+$_['text_general']            = 'General';
+$_['text_access']             = 'Access';
+$_['text_add_keys']           = 'Add Key';
+$_['text_edit_keys']          = 'Edit Key';
+$_['text_type_login']         = 'Generated an access token';
+$_['text_type_refresh_token'] = 'Updated access token';
+
+// Error
+$_['error_permission']        = 'Attention: You do not have permission to modify API Rest!';
+$_['error_description']       = 'The description must be between 3 and 255 characters!';
+$_['error_consumer_key']      = 'The key must be between 7 and 67 characters long and must start with "ck_"!';
+$_['error_consumer_secret']   = 'The secret key must be between 7 and 67 characters long and must start with "cs_"!';
+$_['error_consumer_exist']    = 'The keys informed have already been registered!';
