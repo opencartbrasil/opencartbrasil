602174b4b6b5c0df4573f29c19982f21036e113d
Valdeir S <valdeir@v5market.com>
2021-08-03 17:10:20 -0300

    feat: adiciona tradução e formata arquivo de linguagem
    Signed-off-by: Valdeir S <valdeir@v5market.com>

--- admin/language/pt-br/advanced/webhook.php
+++ admin/language/pt-br/advanced/webhook.php
@@ -1,48 +1,53 @@
 <?php
+//Heading
+$_['heading_title']           = 'Web Hooks';
 
-$_['heading_title'] = 'Web Hooks';
+//Column
+$_['column_description']      = 'Descrição';
+$_['column_status']           = 'Situação';
+$_['column_date_added']       = 'Data de Criação';
+$_['column_date_modified']    = 'Data de Modificação';
+$_['column_actions_hook']     = 'Ações do Gatilho';
+$_['column_header']           = 'Cabeçalho';
+$_['column_create']           = 'Criar';
+$_['column_edit']             = 'Editar';
+$_['column_delete']           = 'Remover';
+$_['column_status_code']      = 'HTTP Status Code';
+$_['column_action']           = 'Ação';
 
-$_['column_description'] = 'Descrição';
-$_['column_status'] = 'Situação';
-$_['column_date_added'] = 'Data de Criação';
-$_['column_date_modified'] = 'Data de Modificação';
-$_['column_actions_hook'] = 'Ações do Gatilho';
-$_['column_header'] = 'Cabeçalho';
-$_['column_create'] = 'Criar';
-$_['column_edit'] = 'Editar';
-$_['column_delete'] = 'Remover';
-$_['column_status_code'] = 'HTTP Status Code';
-$_['column_action'] = 'Ação';
+//Text
+$_['text_list']               = 'Clientes';
+$_['text_form']               = 'Formulário';
+$_['text_data']               = 'Dados';
+$_['text_history']            = 'Histórico';
+$_['text_product']            = 'Produto';
+$_['text_request_id']         = 'Requisição #';
+$_['text_request']            = 'Requisição';
+$_['text_response']           = 'Resposta';
+$_['text_success']            = 'Web Hook alterado com sucesso';
 
-$_['text_list'] = 'Clientes';
-$_['text_form'] = 'Formulário';
-$_['text_data'] = 'Dados';
-$_['text_history'] = 'Histórico';
-$_['text_product'] = 'Produto';
-$_['text_request_id'] = 'Requisição #';
-$_['text_request'] = 'Requisição';
-$_['text_response'] = 'Resposta';
-$_['text_success'] = 'Web Hook alterado com sucesso';
+//Button
+$_['button_edit']             = 'Editar';
+$_['button_logs']             = 'Logs';
+$_['button_disable']          = 'Desabilitar';
+$_['button_enable']           = 'Habilitar';
+$_['button_cancel']           = 'Cancelar';
+$_['button_save']             = 'Salvar';
+$_['button_remove']           = 'Remover';
+$_['button_add_header']       = 'Add Cabeçalho';
 
-$_['button_edit'] = 'Editar';
-$_['button_logs'] = 'Logs';
-$_['button_disable'] = 'Desabilitar';
-$_['button_enable'] = 'Habilitar';
-$_['button_cancel'] = 'Cancelar';
-$_['button_save'] = 'Salvar';
-$_['button_remove'] = 'Salvar';
-$_['button_add_header'] = 'Add Cabeçalho';
+//Entry
+$_['entry_status']            = 'Situação';
+$_['entry_description']       = 'Descrição';
+$_['entry_url']               = 'URL';
+$_['entry_auth']              = 'Autenticação HTTP Basic';
+$_['entry_auth_user']         = 'Usuário (HTTP Basic)';
+$_['entry_auth_password']     = 'Senha (HTTP Basic)';
+$_['entry_headers']           = 'Cabeçalhos';
+$_['entry_headers_example']   = 'Ex.: Content-Type: application/json';
+$_['entry_actions']           = 'Gatilhos';
 
-$_['entry_status'] = 'Situação';
-$_['entry_description'] = 'Descrição';
-$_['entry_url'] = 'URL';
-$_['entry_auth'] = 'Autenticação HTTP Basic';
-$_['entry_auth_user'] = 'Usuário (HTTP Basic)';
-$_['entry_auth_password'] = 'Senha (HTTP Basic)';
-$_['entry_headers'] = 'Cabeçalhos';
-$_['entry_headers_example'] = 'Ex.: Content-Type: application/json';
-$_['entry_actions'] = 'Gatilhos';
-
-$_['error_permission'] = 'Atenção. Você não tem permissão para criar ou editar um cliente do webhook.';
-$_['error_url'] = 'URL inválida';
-$_['error_description'] = 'A descrição deve ter entre 2 e 256 caracteres';
+//Error
+$_['error_permission']        = 'Atenção. Você não tem permissão para criar ou editar um cliente do webhook.';
+$_['error_url']               = 'URL inválida';
+$_['error_description']       = 'A descrição deve ter entre 2 e 256 caracteres';
