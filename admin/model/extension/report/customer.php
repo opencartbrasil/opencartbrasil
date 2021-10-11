950edb305a20514bcaaf1006f9b1523d25d879bb
v5mvidal <manoel@v5market.com>
2021-10-11 16:56:44 -0300

    fix: correção no filtro por data

--- admin/model/extension/report/customer.php
+++ admin/model/extension/report/customer.php
@@ -60,7 +60,7 @@ class ModelExtensionReportCustomer extends Model {
 			);
 		}
 
-		$query = $this->db->query("SELECT COUNT(*) AS total, date_added FROM `" . DB_PREFIX . "customer` WHERE DATE(date_added) >= '" . $this->db->escape(date('Y') . '-' . date('m') . '-1') . "' GROUP BY DATE(date_added)");
+		$query = $this->db->query("SELECT COUNT(*) AS total, date_added FROM `" . DB_PREFIX . "customer` WHERE DATE(date_added) >= DATE('" . $this->db->escape(date('Y') . '-' . date('m') . '-1') . "') GROUP BY DATE(date_added)");
 
 		foreach ($query->rows as $result) {
 			$customer_data[date('j', strtotime($result['date_added']))] = array(
@@ -102,7 +102,7 @@ class ModelExtensionReportCustomer extends Model {
 		}
 
 		if (!empty($data['filter_date_end'])) {
-			$sql .= " AND DATE(o.date_added) <= '" . $this->db->escape($data['filter_date_end']) . "'";
+			$sql .= " AND DATE(o.date_added) <= DATE('" . $this->db->escape($data['filter_date_end']) . "')";
 		}
 
 		if (!empty($data['filter_customer'])) {
@@ -166,11 +166,11 @@ class ModelExtensionReportCustomer extends Model {
 		$sql = "SELECT cr.customer_id, CONCAT(c.firstname, ' ', c.lastname) AS customer, c.email, cgd.name AS customer_group, c.status, SUM(cr.points) AS points, COUNT(o.order_id) AS orders, SUM(o.total) AS total FROM " . DB_PREFIX . "customer_reward cr LEFT JOIN `" . DB_PREFIX . "customer` c ON (cr.customer_id = c.customer_id) LEFT JOIN " . DB_PREFIX . "customer_group_description cgd ON (c.customer_group_id = cgd.customer_group_id) LEFT JOIN `" . DB_PREFIX . "order` o ON (cr.order_id = o.order_id) WHERE cgd.language_id = '" . (int)$this->config->get('config_language_id') . "'";
 
 		if (!empty($data['filter_date_start'])) {
-			$sql .= " AND DATE(cr.date_added) >= '" . $this->db->escape($data['filter_date_start']) . "'";
+			$sql .= " AND DATE(cr.date_added) >= DATE('" . $this->db->escape($data['filter_date_start']) . "')";
 		}
 
 		if (!empty($data['filter_date_end'])) {
-			$sql .= " AND DATE(cr.date_added) <= '" . $this->db->escape($data['filter_date_end']) . "'";
+			$sql .= " AND DATE(cr.date_added) <= DATE('" . $this->db->escape($data['filter_date_end']) . "')";
 		}
 
 		if (!empty($data['filter_customer'])) {
@@ -202,11 +202,11 @@ class ModelExtensionReportCustomer extends Model {
 		$implode = array();
 
 		if (!empty($data['filter_date_start'])) {
-			$implode[] = "DATE(cr.date_added) >= '" . $this->db->escape($data['filter_date_start']) . "'";
+			$implode[] = "DATE(cr.date_added) >= DATE('" . $this->db->escape($data['filter_date_start']) . "')";
 		}
 
 		if (!empty($data['filter_date_end'])) {
-			$implode[] = "DATE(cr.date_added) <= '" . $this->db->escape($data['filter_date_end']) . "'";
+			$implode[] = "DATE(cr.date_added) <= DATE('" . $this->db->escape($data['filter_date_end']) . "')";
 		}
 
 		if (!empty($data['filter_customer'])) {
@@ -232,7 +232,7 @@ class ModelExtensionReportCustomer extends Model {
 		}
 
 		if (!empty($data['filter_date_end'])) {
-			$implode[] = "DATE(ca.date_added) <= '" . $this->db->escape($data['filter_date_end']) . "'";
+			$implode[] = "DATE(ca.date_added) <= DATE('" . $this->db->escape($data['filter_date_end']) . "')";
 		}
 
 		if (!empty($data['filter_customer'])) {
@@ -272,11 +272,11 @@ class ModelExtensionReportCustomer extends Model {
 		$implode = array();
 
 		if (!empty($data['filter_date_start'])) {
-			$implode[] = "DATE(ca.date_added) >= '" . $this->db->escape($data['filter_date_start']) . "'";
+			$implode[] = "DATE(ca.date_added) >= DATE('" . $this->db->escape($data['filter_date_start']) . "')";
 		}
 
 		if (!empty($data['filter_date_end'])) {
-			$implode[] = "DATE(ca.date_added) <= '" . $this->db->escape($data['filter_date_end']) . "'";
+			$implode[] = "DATE(ca.date_added) <= DATE('" . $this->db->escape($data['filter_date_end']) . "')";
 		}
 
 		if (!empty($data['filter_customer'])) {
@@ -302,11 +302,11 @@ class ModelExtensionReportCustomer extends Model {
 		$implode = array();
 
 		if (!empty($data['filter_date_start'])) {
-			$implode[] = "DATE(cs.date_added) >= '" . $this->db->escape($data['filter_date_start']) . "'";
+			$implode[] = "DATE(cs.date_added) >= DATE('" . $this->db->escape($data['filter_date_start']) . "')";
 		}
 
 		if (!empty($data['filter_date_end'])) {
-			$implode[] = "DATE(cs.date_added) <= '" . $this->db->escape($data['filter_date_end']) . "'";
+			$implode[] = "DATE(cs.date_added) <= DATE('" . $this->db->escape($data['filter_date_end']) . "')";
 		}
 
 		if (!empty($data['filter_keyword'])) {
@@ -350,11 +350,11 @@ class ModelExtensionReportCustomer extends Model {
 		$implode = array();
 
 		if (!empty($data['filter_date_start'])) {
-			$implode[] = "DATE(cs.date_added) >= '" . $this->db->escape($data['filter_date_start']) . "'";
+			$implode[] = "DATE(cs.date_added) >= DATE('" . $this->db->escape($data['filter_date_start']) . "')";
 		}
 
 		if (!empty($data['filter_date_end'])) {
-			$implode[] = "DATE(cs.date_added) <= '" . $this->db->escape($data['filter_date_end']) . "'";
+			$implode[] = "DATE(cs.date_added) <= DATE('" . $this->db->escape($data['filter_date_end']) . "')";
 		}
 
 		if (!empty($data['filter_keyword'])) {
